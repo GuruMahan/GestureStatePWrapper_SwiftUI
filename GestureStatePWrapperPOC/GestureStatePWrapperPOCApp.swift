c096d3fff086bf60c161f0d253ebd24efbca1733
GuruMahan <iosdeve2022@gmail.com>
2023-02-17 12:23:06 +0530

    Initial Commit

---
+++ GestureStatePWrapperPOC/GestureStatePWrapperPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureStatePWrapperPOCApp.swift
+//  GestureStatePWrapperPOC
+//
+//  Created by Guru Mahan on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GestureStatePWrapperPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
